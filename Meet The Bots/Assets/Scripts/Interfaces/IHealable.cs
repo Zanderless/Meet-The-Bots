b82b441b51af0308b1b1be5adf835ecfab276030
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-06-29 14:11:35 -0500

    update code structure

--- Meet The Bots/Assets/Scripts/Interfaces/IHealable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IHealable
-{
-    void AddHealth(float h);
-    void AddArmor(float a);
-}
---
+++ source/Assets/Scripts/Enemy/Dummy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = maxHealth;
+    }
+}
