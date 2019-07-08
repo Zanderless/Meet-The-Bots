9d63b1f2a06cd6240fb5dc213eb00d0e3b9553d0
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-08 18:08:24 -0500

    S-0008: AI System groundwork

---
+++ source/Assets/AI/Agent.cs
@@ -0,0 +1,17 @@
+﻿/**
+ * @file       : Dummy.cs
+ * @author     :
+ * @description:
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent {
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = maxHealth;
+    }
+}
