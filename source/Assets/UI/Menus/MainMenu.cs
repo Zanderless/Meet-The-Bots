5b569fdc50cfe691bb599416bbd99a3feb380425
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-08 17:54:24 -0500

    UI System groundwork

---
+++ source/Assets/UI/Menus/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿/**
+ * @file       : MainMenu.cs
+ * @author     :
+ * @description: MainMenu
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu {
+    
+}
