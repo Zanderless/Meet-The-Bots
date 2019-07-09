765c967ed885ef139932671e9c3666da4ad1f20a
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-08 20:02:55 -0500

    game managers update

---
+++ source/Assets/Manager/GameManager.cs
@@ -0,0 +1,17 @@
+/**
+ * @file       : GameManager.cs
+ * @author     :
+ * @description: Core Game manager
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager {
+
+}
+
+public class GameSingleton {
+    
+}
---
+++ source/Assets/Manager/GameSingleton.cs
@@ -0,0 +1,13 @@
+/**
+ * @file       : GameSingleton.cs
+ * @author     :
+ * @description: Core Game manager
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSingleton {
+    
+}
