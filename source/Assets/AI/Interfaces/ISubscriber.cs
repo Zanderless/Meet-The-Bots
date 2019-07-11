13087e082bd648a96fb13002c9f020282cd40302
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-11 08:26:53 -0500

    adding global namespace

---
+++ source/Assets/AI/Interfaces/ISubscriber.cs
@@ -0,0 +1,16 @@
+/**
+ * @file       : ISubscriber.cs
+ * @author     :
+ * @description:
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+
+    interface ISubscriber {
+        
+    }
+}
