d810ddd7cbfd9cc2525a39e6231cdb3a27899b97
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-11 06:47:45 -0500

    more update changes

--- source/Assets/AI/Agents/Agent.cs
+++ source/Assets/AI/Agents/Agent.cs
@@ -8,9 +8,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseAgent {
-    // Start is called before the first frame update
-    void Start() {
+namespace MTB {
 
+    public class BaseAgent : IAgent {
+        
     }
 }
--- source/Assets/Characters/Base/Entity.cs
+++ source/Assets/Characters/Base/Entity.cs
@@ -9,95 +9,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct Attributes
-{
-    public readonly double heatlh;
-    public readonly decimal stamina;
-    public readonly decimal pysche;
+namespace MTB {
 
-    public MyStruct(double health, decimal stamina, decimal pysche)
+    public class Entity : MonoBehaviour, IEntity
     {
-        this.health = health;
-        this.stamina = stamina;
-        this.pysche = pysche;
-    }
-}
-
-
-public class Entity : MonoBehaviour
-{
-  
-    /* Personality */
     
+        /* Personality */
+        
+
+        /* Attributes */
+        private Attributes attributes;
+
+        public attributes Attributes {
+            get { return attributes; }
+            set { attributes = attributesData; }
+        }
 
 
-    /* Attributes */
-    private Attributes attributes;
-
-    public attributes Attributes
-    {
-        get { return attributes; }
-        set { attributes = attributesData; }
-    }
-
-    // private float _health;
-    // private float _stamina;
-    // private float _psyche;
-
-    public float maxHealth;
-    public float maxStamina; 
-    
-    public float maxPsyche;
-    
-    private float _armor;
-
-    public float maxArmor;
-
-
-    public float Health
-    {
-        get { return _health; }
-        set
-        {
-            _health = value;
-            _health = Mathf.Clamp(_health, 0, maxHealth);
-
-            if (Health < 0)
-                Die();
+        public Entity() {
+            attributes = new Attributes(maxHealth, maxStamina, maxPsyche);
         }
     }
-
-    public float Armor
-    {
-        get { return _armor; }
-        set
-        {
-            _armor = value;
-            _armor = Mathf.Clamp(_armor, 0, maxArmor);
-        }
-    }
-
-    public bool IsAlive => Health > 0;
-
-    /* Entity Pose */
-    public Vector3 EnityPos => transform.position;
-    public Quaternion EntityRot => transform.rotation;
-
-    /* Life states */
-    public virtual void Die()
-    {
-        print("dead");
-    }
-
-    public virtual void Fatigue()
-    {
-        print("fatigued");
-    }
-
-
-    public Entity()
-    {
-        attributes = new Attributes(maxHealth, maxStamina, maxPsyche);
-    }
-
 }
