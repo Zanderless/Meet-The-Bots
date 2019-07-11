d4db43ecf525f7b89ff0961f12ca87d848f0b7a1
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-11 08:30:20 -0500

    adding global namespace

---
+++ source/Assets/AI/Interfaces/IPublisher.cs
@@ -0,0 +1,16 @@
+/**
+ * @file       : IPublisher.cs
+ * @author     :
+ * @description:
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+
+    interface IPublisher {
+        
+    }
+}
