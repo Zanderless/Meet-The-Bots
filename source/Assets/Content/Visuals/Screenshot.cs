b9385af5eb0069d935620c9c4fa100e52bd05aed
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-11 07:56:23 -0500

    adding global namespace

---
+++ source/Assets/Content/Visuals/Screenshot.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * @file       : Screenshot.cs
+ * @author     :
+ * @description:
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+    
+    public class Screenshot : MonoBehaviour {
+
+        /**
+         * @brief
+         */
+        private void Update() {
+
+            if (Application.isEditor)  {
+                if (Input.GetKeyDown(KeyCode.F1)) {
+                    UnityEngine.ScreenCapture.CaptureScreenshot("Screenshot_" + Random.Range(0, 100).ToString() + ".png");
+                }
+            }
+        }
+    }
+}
