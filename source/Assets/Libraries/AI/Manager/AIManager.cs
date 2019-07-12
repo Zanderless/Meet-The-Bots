a4d776489c4f87d76efddb49f51ad74500fc808d
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-11 20:44:52 -0500

    adding global namespace

---
+++ source/Assets/Libraries/AI/Manager/AIManager.cs
@@ -0,0 +1,17 @@
+/**
+ * @file       : AIManager.cs
+ * @author     :
+ * @description: Core Game manager
+ * @note    
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+    
+    public class AIManager {
+
+    }
+}
---
+++ source/Assets/Libraries/ProcGen/Manager/ProcGenManager.cs
@@ -0,0 +1,17 @@
+/**
+ * @file       : ProcGenManager.cs
+ * @author     :
+ * @description: Core Game manager
+ * @note    
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+    
+    public class ProcGenManager {
+
+    }
+}
