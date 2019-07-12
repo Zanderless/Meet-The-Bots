11b06c62c664c5bca8c3d16dc7239151f1eddb61
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-11 20:44:52 -0500

    adding global namespace

---
+++ source/Assets/Libraries/ProcGen/Interfacees/IProcGen.cs
@@ -0,0 +1,17 @@
+/**
+ * @file       : IProcGen.cs
+ * @author     :
+ * @description: Core Game manager
+ * @note    
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+    
+    interface IProcGen {
+
+    }
+}
