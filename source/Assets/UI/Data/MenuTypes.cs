06e23438f07e8fd5804aef643fe62b9c1de20497
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-15 18:59:37 -0500

    add basic UI data types

---
+++ source/Assets/UI/Data/MenuTypes.cs
@@ -0,0 +1,20 @@
+/**
+ * @file       : MenuTypes.cs
+ * @author     :
+ * @description: Menu types and data structures
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+
+    public class MenuTypes {
+
+    }
+
+    public struct menuData  {
+
+    }
+}
---
+++ source/Assets/UI/Data/Symbology.cs
@@ -0,0 +1,16 @@
+/**
+ * @file       : Symbology.cs
+ * @author     :
+ * @description: UI symbology data 
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+
+    public class Symbology {
+
+    }
+}
