04287a54b476be2091a41dfa379db5f2274120b6
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-15 21:37:08 -0500

    add character controller

---
+++ source/Assets/Characters/Player/BaseController.cs
@@ -0,0 +1,20 @@
+/**
+ * @file       : Player.cs
+ * @author     :
+ * @description:
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.ProceduralImage;
+using TMPro;
+using System;
+
+namespace MTB {
+
+    public class BaseController : CharacterController  {
+
+    }
+
+}
