6e6c5f5b5495dca8faa4ff8860e17a3c43e7f4ac
dominic-eaton10 <domnic_eaton@yahoo.com>
2019-07-15 20:00:49 -0500

    add basic audio types

---
+++ source/Assets/Content/Audio/Data/AudioTypes.cs
@@ -0,0 +1,26 @@
+/**
+ * @file       : AudioTypes.cs
+ * @author     :
+ * @description:
+ * @note    
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MTB {
+    /* Audio enums */
+    public static enum AudioState {
+        ENABLED,
+        MUTED,
+    }
+
+    /* Audio data structs */
+    public struct audioData {
+        double volume;
+    }
+
+    public class AudioTypes {
+
+    }
+}
