5a65267c27e75c1c0de0cb7af133237e48990955
ItsKhanny <peckjakob54@gmail.com>
2019-07-24 12:16:39 -0700

    Created Resources folder and basic Json file for weapons

--- source/Assets/Libraries/Combat/Weapons/WeaponBase.cs
+++ source/Assets/Libraries/Combat/Weapons/WeaponBase.cs
@@ -8,58 +8,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MTB
+public class WeaponBase : MonoBehaviour
 {
-    public class WeaponBase : MonoBehaviour
-    {
+    
+    
 
-        //Public Variables
-        [Tooltip("Insert path to weapon json file. Don't include Assets or Resources folder \n EX. Weapon/Data/Pistol")]
-        public string dataPath;
+}
 
-        //private variables
-        private WeaponInfo info;
+public class WeaponInfo
+{
 
-        private void Start()
-        {
-            LoadData();
-        }
+    public float MaxAmmo { get; set; }
+    public float MaxStoredAmmo { get; set; }
+    public float FireRate { get; set; }
+    public decimal Damage { get; set; }
+    public float ShotDistance { get; set; }
+    public string WeaponPrefab { get; set; }
+    public bool IsAutomatic { get; set; }
+    public bool UsesAmmo { get; set; }
+    public bool UsesProjectile { get; set; }
+    public string ProjectilePrefab { get; set; }
 
-        void LoadData()
-        {
-
-            TextAsset text = Resources.Load(dataPath) as TextAsset;
-
-            try
-            {
-                info = JsonUtility.FromJson<WeaponInfo>(text.text);
-            }
-            catch
-            {
-                Debug.LogError("Cannot find file from path");
-            }
-
-            print(info.WeaponName);
-
-        }
-
-    }
-
-    [System.Serializable]
-    public class WeaponInfo
-    {
-
-        public string WeaponName;
-        public float MaxAmmo;
-        public float MaxStoredAmmo;
-        public float FireRate;
-        public float Damage;
-        public float ShotDistance;
-        public string WeaponPrefab;
-        public bool IsAutomatic;
-        public bool UsesAmmo;
-        public bool UsesProjectile;
-        public string ProjectilePrefab;
-
-    }
 }
