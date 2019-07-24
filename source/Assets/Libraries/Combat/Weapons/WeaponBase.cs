a0e8fd993ed363e2f1c6f646f9a6fb74e0bddab0
ItsKhanny <peckjakob54@gmail.com>
2019-07-24 12:16:39 -0700

    Created Resources folder and basic Json file for weapons

---
+++ source/Assets/Libraries/Combat/Weapons/WeaponBase.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * @file       : Entity.cs
+ * @author     : Jakob P.
+ * @description: The script that handles all weapon logic
+ * @note       : This script belongs on the actual gun/fps arms prefab
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase : MonoBehaviour
+{
+    
+    
+
+}
+
+public class WeaponInfo
+{
+
+    public float MaxAmmo { get; set; }
+    public float MaxStoredAmmo { get; set; }
+    public float FireRate { get; set; }
+    public decimal Damage { get; set; }
+    public float ShotDistance { get; set; }
+    public string WeaponPrefab { get; set; }
+    public bool IsAutomatic { get; set; }
+    public bool UsesAmmo { get; set; }
+    public bool UsesProjectile { get; set; }
+    public string ProjectilePrefab { get; set; }
+
+}
